0dc86c3551ba59b9f705de24e6df4a759301f0b7
fabienoger <fab-oger@live.fr>
2017-11-12 14:31:59 +0100

    Add base layout (header, footer)

--- components/nav-header.vue
+++ components/nav-header.vue
@@ -1,5 +1,42 @@
 <template>
-  <header class="">
-    Liberte Esthetique
+  <header class="nav-header">
+    <div class="layout">
+      <div class="layout-title">
+        Liberté Esthetique
+      </div>
+        <Menu mode="horizontal" theme="light" active-name="1">
+            <div class="layout-nav">
+                <MenuItem name="1">
+                    <Icon type="home"></Icon>
+                    Accueil
+                </MenuItem>
+                <MenuItem name="2">
+                    <Icon type="email"></Icon>
+                    Contact
+                </MenuItem>
+            </div>
+        </Menu>
+    </div>
   </header>
 </template>
+
+<style lang="scss" scoped>
+@import "~assets/css/variables.scss";
+
+.layout {
+  border: 1px solid #d7dde4;
+  background: #f5f7f9;
+
+  .layout-title {
+    background-color: $primary-color;
+    color: $primary-color-inverse;
+    padding: 15px 0;
+    text-align: center;
+    font-size: 2em;
+  }
+  .layout-nav {
+    width: 220px;
+    margin: 0 auto;
+  }
+}
+</style>
