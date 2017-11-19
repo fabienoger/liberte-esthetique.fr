ed3a46e99b19318af6005e524bf74a3d9300b262
fabienoger <fab-oger@live.fr>
2017-11-19 03:23:01 +0100

    Do not use Tag component

--- components/informations.vue
+++ components/informations.vue
@@ -1,28 +1,30 @@
 <template>
-  <Card>
-    <p slot="title">Coordonnées</p>
-    <p>Content of card</p>
-    <p>
-      <Tag>
-        <Icon type="email"></Icon>
-        Email
-      </Tag>
-      contact@contact.com
-    </p>
-    <p>
-      <Tag>
-        Adresse
-      </Tag>
-      <span>30 Ter Rue de la Liberté</span> 94300 Vincennes, France
-    </p>
-    <p>
-      <Tag>
-        <Icon type="ios-telephone"></Icon>
-        Téléphone
-      </Tag>
-      +1 555 123456
-    </p>
-  </Card>
+  <div>
+    <Card>
+      <p slot="title">Coordonnées</p>
+      <p>
+        <div class="ivu-tag ivu-tag-checked">
+          <Icon type="email"></Icon>
+          Email
+        </div>
+        contact@contact.com
+      </p>
+      <p>
+        <div class="ivu-tag ivu-tag-checked">
+          <Icon type="location"></Icon>
+          Adresse
+        </div>
+        <span>30 Ter Rue de la Liberté</span> 94300 Vincennes, France
+      </p>
+      <p>
+        <div class="ivu-tag ivu-tag-checked">
+          <Icon type="ios-telephone"></Icon>
+          Téléphone
+        </div>
+        01 77 01 51 58
+      </p>
+    </Card>
+  </div>
 </template>
 
 <script>
