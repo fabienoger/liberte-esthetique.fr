323dc2104dfc766973d118979f991f996f6127a2
Fabien Oger <foger@synaltic.fr>
2018-05-16 21:35:27 +0200

    Configuration

--- nuxt.config.js
+++ nuxt.config.js
@@ -42,7 +42,7 @@ module.exports = {
     '@nuxtjs/toast'
   ],
   axios: {
-    baseUrl: process.env.BASE_URL || 'http://localhost:3000'
+    browserBaseURL: process.env.BASE_URL || 'http://localhost:3000'
   },
   toast: {
     position: 'top-right',
